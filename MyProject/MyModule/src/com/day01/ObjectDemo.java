164811556e61c0e350517e18d7e3ee566109cda7
guodong <guodong_1980@163.com>
2018-12-18 22:40:56 +0800

    我的第二次提交

--- MyProject/MyModule/src/com/day01/ObjectDemo.java
+++ MyProject/MyModule/src/com/day01/ObjectDemo.java
@@ -41,8 +41,7 @@ package com.day01;
 
 public class ObjectDemo {
     public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
+
         System.out.println("你好");
         System.out.println("我是郭栋");
         System.out.println("我是郭栋");
