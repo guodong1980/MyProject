a9dce41e15202a7620a465f4e7b0bd6510e166fe
guodong <guodong_1980@163.com>
2018-12-18 22:47:16 +0800

    我的第二次提交

--- MyProject/MyModule/src/com/day01/ObjectDemo.java
+++ MyProject/MyModule/src/com/day01/ObjectDemo.java
@@ -43,6 +43,7 @@ public class ObjectDemo {
     public static void main(String[] args) {
 
         System.out.println("你好");
+        System.out.println("你好");
         System.out.println("我是郭栋");
         System.out.println("我是郭栋");
         System.out.println("我是郭栋");
