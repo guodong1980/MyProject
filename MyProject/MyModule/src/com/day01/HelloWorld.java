a1d058205e889c829de764ce18e262afb51e49f1
guodong <guodong_1980@163.com>
2018-12-22 12:27:16 +0800

    我的第15次提交

--- MyProject/MyModule/src/com/day01/HelloWorld.java
+++ MyProject/MyModule/src/com/day01/HelloWorld.java
@@ -8,9 +8,6 @@ package com.day01;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("你好世界!");
-        System.out.println("你好世界!");
-        System.out.println("你好世界!");
-        System.out.println("你好世界!");
+        System.out.println("我是郭栋");
     }
 }
