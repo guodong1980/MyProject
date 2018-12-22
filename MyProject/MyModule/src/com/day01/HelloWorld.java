2d6534cc5a2d056faab6f92cf8ce5211e9a84c08
guodong <guodong_1980@163.com>
2018-12-22 15:34:09 +0800

    我添加了day01新的内容

--- MyProject/MyModule/src/com/day01/HelloWorld.java
+++ MyProject/MyModule/src/com/day01/HelloWorld.java
@@ -1,14 +1,30 @@
 package com.day01;
 
-/**
- @author 郭栋
- @create 2018-12-22 10:05
+/*
+ * 1.源文件以.java结尾
+ * 2.源文件中可以有多个class声明的类
+ * 3.类中可以有主方法,其格式固定  public static void main(String[] args){}
+ * 4一个源文件中只能有一个声明为public的类,同时要求此类的名与源文件名一致
+ * 5.每个语句都以";"结束
+ * 6.注释:单行,多行,文档
+ *
+ *
  *
  *
  */
+
+/**
+ * 功能描述: Hello World
+ * @classname:HelloWorld
+ * @since: 1.0.0
+ * @Author:guodong
+ * @Date:2018-12-22
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("我是郭栋");
+        System.out.println("你好,世界!");
         System.out.println("Hello Java");
     }
 }
