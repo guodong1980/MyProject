c944d1505a4fc96fb2e5793c04ef02c6081ebe14
guodong <guodong_1980@163.com>
2018-12-22 15:09:08 +0800

    我添加了输出Java语句

--- MyProject/MyModule/src/com/day01/HelloWorld.java
+++ MyProject/MyModule/src/com/day01/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("我是郭栋");
         System.out.println("你好,世界!");
+        System.out.println("Hello Java");
     }
 }
