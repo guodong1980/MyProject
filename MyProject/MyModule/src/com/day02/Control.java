c84bcfc647258c52e5e373d14d17c8f36ce7c7a9
guodong <guodong_1980@163.com>
2018-12-23 13:37:33 +0800

    day02最新推送02

--- MyProject/MyModule/src/com/day02/Control.java
+++ MyProject/MyModule/src/com/day02/Control.java
@@ -4,9 +4,10 @@ package com.day02;
  * 1.顺序结构:从上往下
  * 2.条件分支:
  *   (1)if-else
- *      1)if(true) { }
+ *      1)if(条件判断) { }
  *      2)if(条件判断){ }else{ }
- *      3)if(条件判断){ }else if{ }...else{ }
+ *      3)if(条件判断){ }else if(条件判断){ }...else{ }
+ *         如果多个条件之间存在"包含关系",要求范围小的写在范围大的上面
  *   (2)switch-case
  * 3.循环结构:
  *   (1)while
@@ -19,4 +20,7 @@ package com.day02;
  *
  */
 public class Control {
+    public static void main(String[] args) {
+
+    }
 }
