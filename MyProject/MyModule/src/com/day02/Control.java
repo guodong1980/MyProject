92e5af3e61796c30c7c47edf9c24feb2a264d980
guodong <guodong_1980@163.com>
2018-12-23 14:03:55 +0800

    day02最新推送02

--- MyProject/MyModule/src/com/day02/Control.java
+++ MyProject/MyModule/src/com/day02/Control.java
@@ -17,6 +17,11 @@ package com.day02;
  *                  break;
  *              }
  *      2)特性:根据表达式的值,选择相应的case去判断,一旦满足case条件,就执行性case的相应的语句,注意case穿透
+ *              default是可以选的,位置是灵活的
+ *              case 值1:其中值1,只能是固定单个值,不能是范围
+ *      3)表达式类型:只能是 char  byte  short  int  枚举  String
+ *
+ *
  *
  * 3.循环结构:
  *   (1)while
