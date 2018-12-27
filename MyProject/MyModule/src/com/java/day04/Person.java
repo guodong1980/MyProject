a27506789df03cb617328ddcb0e682b54bf73e84
guodong <guodong_1980@163.com>
2018-12-27 18:41:28 +0800

    day02最新推送06

---
+++ MyProject/MyModule/src/com/java/day04/Person.java
@@ -0,0 +1,37 @@
+package com.java.day04;
+
+/**
+ @作者 郭栋
+ @日期 2018-12-26
+ *
+ *
+ */
+class Person {
+    //1.属性
+    String name;
+    int age;
+    boolean sex;
+
+    //2.方法
+    public void eat() {
+        System.out.println("人吃饭");
+    }
+
+    public void sleep() {
+        System.out.println("人睡觉");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void info() {
+        eat();
+        sleep();
+        System.out.println("name=" + name + '\t' + "age=" + age + '\t' + "sex=" + sex);
+    }
+}
