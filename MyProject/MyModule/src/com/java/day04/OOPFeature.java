814b5d9de2f7bed36a5802551bb2bf767e01c03a
guodong <guodong_1980@163.com>
2018-12-30 16:22:00 +0800

    day04最新推送01

---
+++ MyProject/MyModule/src/com/java/day04/OOPFeature.java
@@ -0,0 +1,16 @@
+package com.java.day04;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 
+ * User: guodong
+ * Date: 2018-12-30
+ * 面向对象
+ * 1.隐藏和封装
+ *   (1)当创建了类的对象以后,如果直接通过对象.属性的方式赋值,会出现意外,一般通过对象.方法的形式来访问
+ *   (2)
+ *
+ *
+ */
+public class OOPFeature {
+}
