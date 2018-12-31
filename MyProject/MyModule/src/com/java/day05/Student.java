16c9c4915f7b9e357c8c1fe88d4256d61c67c8cd
guodong <guodong_1980@163.com>
2018-12-31 11:50:31 +0800

    day06最新推送02

---
+++ MyProject/MyModule/src/com/java/day05/Student.java
@@ -0,0 +1,17 @@
+package com.java.day05;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 
+ * User: guodong
+ * Date: 2018-12-31
+ */
+public class Student extends Person {
+    Student(int age, String name) {
+        super(age, name);
+    }
+
+    public Student(String name) {
+        super(name);
+    }
+}
