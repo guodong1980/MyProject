ee7340c3fed5f97c41bc42bcd1fba57970935baf
guodong <guodong_1980@163.com>
2019-01-01 19:30:47 +0800

    day07最新推送02

---
+++ MyProject/MyModule/src/com/java/day06/常用类.java
@@ -0,0 +1,16 @@
+package com.java.day06;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guodong
+ * Date: 2019-01-01
+ * Description:常用类笔记
+ * 常用类:
+ *  1.Object类
+ *    (1)equals方法与==的区别
+ *       1)==:两端是基本数据类型比较的是本身值,两端是引用类型比较的是地址值是否相等
+ *       2)equals方法: a.只能处理引用类型比较,不能处理基本类型比较
+ *                     b.String类,包装类,file类,Date类,这些类重写了Object类的equals()方法,比较对象的实体内容是否相同
+ *　　　
+ *
+ */
